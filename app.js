0c02c1dec891eadaae7527f8623605123b2028be
Emmanuel Iyere <40471024+emmanuelky@users.noreply.github.com>
2021-08-15 15:39:26 +0200

    displayed city, weather and temp

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-window.onload = () => {
-    getData()
-}
+const btn = document.querySelector(".btn")
+const query = document.querySelector(".value")
+const city = document.querySelector(".city")
+const temp = document.querySelector(".temperature")
 
 
-
-const getData = (query) => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=2e5f08bfe5cce317674a1031f2d125e8`)
+btn.addEventListener("click", () => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=` + query.value + `&appid=2e5f08bfe5cce317674a1031f2d125e8`)
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => {
+
+            city.innerHTML = `<h1 class="my-5 mx-5">${data.name}</h1>`
+
+            temp.innerHTML = `<h4 class="mt-5">${data.weather[0].main}</h4>
+                                 <h6 class="mb-4">(${data.weather[0].description})</h6>
+                               <h4>Temperature:<h1>${data.main.temp.toFixed() + '&deg' + 'c'}</h1></h4>`
+
+
+            console.log(data)
+        })
         .catch((error) => error)
-}
+})
+
+
+//clouds, rain, clear, 
+
+
